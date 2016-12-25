9e6650cf81fbf03dbf1d764836854740b642b889
sarvajeet <sarvajeet@mobisir.net>
2016-12-25 12:21:26 +0530

    Numpy Floor Ceil and Rint HackerRank Problem

---
+++ floor_ceil_rint.py
@@ -0,0 +1,7 @@
+# https://www.hackerrank.com/challenges/floor-ceil-and-rint
+
+import numpy
+lst = numpy.array(list(map(float,input().split())))
+print (numpy.floor(lst))
+print (numpy.ceil(lst))
+print (numpy.rint(lst))
