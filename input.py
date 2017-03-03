c512f4aaa88386956d50775143587977e51bab77
sarvajeet <sarvajeet@mobisir.net>
2017-03-04 01:22:25 +0530

    Input

---
+++ input.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+x,k = raw_input().split()
+x,k = int(x), int(k)
+st = input()
+if st == k:
+    print True
+else:
+    print False
